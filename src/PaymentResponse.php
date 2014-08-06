cf2d75e15e06d1d0004f74f36183ef6d6a70d4b5
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2014-08-06 10:20:31 +0300

    Исправлены пространства имен

--- src/PaymentResponse.php
+++ src/PaymentResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace iiifx\Qiwi;
+namespace iiifx\Component\Payment\Qiwi;
 
 /**
  * Class PaymentResponse
--- src/_example.php
+++ src/_example.php
@@ -21,15 +21,15 @@ $orderAmount = 1.00;
 ### Выставляем счет платежа ###################################################
 #
 
-$qiwiPaymentRequest = new \iiifx\Qiwi\PaymentRequest( $shopId, $apiId, $apiPassword );
+$qiwiPaymentRequest = new \iiifx\Component\Payment\Qiwi\PaymentRequest( $shopId, $apiId, $apiPassword );
 # Задаем данные платежа
 $qiwiPaymentRequest
     ->setClientPhone( $clientPhone ) # Телефон клиента: +380683626568
     ->setPaymentAmount( $orderAmount ) # Сумма оплаты: 111.00
     ->setCurrencyCode( $currencyCode ) # Валюта, трехсимвольный код ISO4217: RUB
     ->setPaymentComment( "Оплата заказа #{$orderID}" ) # Комментарий к оплате
-    ->setLifetimeSeconds( \iiifx\Qiwi\PaymentRequest::LifetimeSeconds_OneDay ) # Время жизни счета, в секундах
-    ->setPaymentType( \iiifx\Qiwi\PaymentRequest::PaymentType_Any ) # Тип оплаты
+    ->setLifetimeSeconds( \iiifx\Component\Payment\Qiwi\PaymentRequest::LifetimeSeconds_OneDay ) # Время жизни счета, в секундах
+    ->setPaymentType( \iiifx\Component\Payment\Qiwi\PaymentRequest::PaymentType_Any ) # Тип оплаты
     ->setShopName( 'WePlay' );
 if ( !$qiwiPaymentRequest->validateData() ) {
     # Ошибка данных оплаты
@@ -51,7 +51,7 @@ if ( !$qiwiPaymentRequest->validateData() ) {
 ### Проверяем статус платежа ##################################################
 #
 
-$qiwiCheckRequest = new \iiifx\Qiwi\CheckRequest( $shopId, $apiId, $apiPassword );
+$qiwiCheckRequest = new \iiifx\Component\Payment\Qiwi\CheckRequest( $shopId, $apiId, $apiPassword );
 # Устанавливаем номер транзакции
 $qiwiCheckRequest->setTransactionId( 'WP-140805-181439-386' /*$transactionId*/ );
 # Делаем запрос, получаем ответ
